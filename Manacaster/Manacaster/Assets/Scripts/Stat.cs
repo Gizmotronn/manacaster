e286db6dc9a85775202b284cb209e9ce371a93a3
Liam Arbuckle <droidology@allianceofdroids.org.au>
2020-03-07 21:49:42 +0800

    https://acord.software/stellarios/inscoperpg2dot1

--- Manacaster/Manacaster/Assets/Scripts/Stat.cs
+++ Manacaster/Manacaster/Assets/Scripts/Stat.cs
@@ -5,69 +5,111 @@ using UnityEngine.UI;
 
 public class Stat : MonoBehaviour
 {
+
+    /// <summary>
+    /// The actual image that we are chaing the fill of
+    /// </summary>
     private Image content;
 
+    /// <summary>
+    /// A reference to the value text on the bar
+    /// </summary>
+    [SerializeField]
+    private Text statValue;
+
+    /// <summary>
+    /// Hold the current fill value, we use this, so that we know if we need to lerp a value
+    /// </summary>
+    private float currentFill;
+
+    /// <summary>
+    /// The lerp speed
+    /// </summary>
     [SerializeField]
     private float lerpSpeed;
 
-    private float currentFill;
+    /// <summary>
+    /// The stat's maxValue for example max health or mana
+    /// </summary>
+    public float MyMaxValue { get; set; }
 
-    public float MyMaxValue { get; set; } // Max value for health, game objects
+    /// <summary>
+    /// The currentValue for example the current health or mana
+    /// </summary>
+    private float currentValue;
 
-    private float currentValue; 
-
-    public float MyCurrentValue // property for accessing from other scripts.
+    /// <summary>
+    /// Proprty for setting the current value, this has to be used every time we change the currentValue, so that everything updates correctly
+    /// </summary>
+    public float MyCurrentValue
     {
-        get{
-            return currentValue; // Read current value // Check /Hashtag/
+        get
+        {
+
+            return currentValue;
         }
 
-        set{
-            if (value > MyMaxValue)
+        set
+        {
+            if (value > MyMaxValue)//Makes sure that we don't get too much health
             {
                 currentValue = MyMaxValue;
             }
-
-            else if (value < 0)
+            else if (value < 0) //Makes sure that we don't get health below 0
             {
                 currentValue = 0;
             }
-            else
+            else //Makes sure that we set the current value withing the bounds of 0 to max health
             {
                 currentValue = value;
             }
 
-            // currentValue = value; // Value (RHS) set in player.cs // See https://acord-robotics.github.io/stellarios/ar-11-healthmanabar2dot0/
+            //Calculates the currentFill, so that we can lerp
             currentFill = currentValue / MyMaxValue;
-        
+
+            //Makes sure that we update the value text
+            statValue.text = currentValue + " / " + MyMaxValue;
         }
     }
 
-    // Start is called before the first frame update
+
+
+
+    // Use this for initialization
     void Start()
     {
-        MyMaxValue = 100; // Prevents the health/mana going above 100
-        
-        content = GetComponent<Image>(); // The variable "content" is connected/references the "health", "mana" game objects in scene: "scene"
+        //Creates a reference to the content
+        content = GetComponent<Image>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (currentFill != content.fillAmount){
-            content.fillAmount = Mathf.Lerp(content.fillAmount,currentFill, Time.deltaTime * lerpSpeed); // Moves equally on every single device // Smooth transitions
-        }
-        
-
-        Debug.Log(MyCurrentValue);
-
-        content.fillAmount = currentFill;
+        //Makes sure that we update the bar
+        HandleBar();
     }
 
-    // Initialize for stat values
+    /// <summary>
+    /// Initialises the bar
+    /// </summary>
+    /// <param name="currentValue">The current value of the bar</param>
+    /// <param name="maxValue">The max value of the bar</param>
     public void Initialize(float currentValue, float maxValue)
     {
         MyMaxValue = maxValue;
-        MyMaxValue = currentValue;
+        MyCurrentValue = currentValue;
+    }
+
+    /// <summary>
+    /// Makes sure that the bar updates
+    /// </summary>
+    private void HandleBar()
+    {
+        if (currentFill != content.fillAmount) //If we have a new fill amount then we know that we need to update the bar
+        {
+            //Lerps the fill amount so that we get a smooth movement
+            content.fillAmount = Mathf.Lerp(content.fillAmount, currentFill, Time.deltaTime * lerpSpeed);
+        }
+
     }
 }

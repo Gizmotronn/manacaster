0b5a4b2bccf27a7121a701988ef3c821d1398cd4
Liam Arbuckle <droidology@allianceofdroids.org.au>
2020-03-22 09:30:40 +0800

    Closing down work on this - see prev commit
    78375d0466411b8f14b44906fa795f452f019695

---
+++ SpaceShooter/2D Space Shooter/Script/spaceshipScript.js
@@ -0,0 +1,13 @@
+﻿// Function called about 60 times per second
+function Update() {
+    // Get the rigidbody component
+    var r2d = GetComponent("Rigidbody2D");
+
+    // Move the spaceship when an arrow key is pressed
+    if (Input.GetKey("right"))
+        r2d.velocity.x = 10;
+    else if (Input.GetKey("left"))
+        r2d.velocity.x = -10;
+    else
+        r2d.velocity.x = 0;
+} 
